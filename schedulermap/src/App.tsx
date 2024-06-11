565ea15671e8174b02482675e93f718afc8b3814
epsekim01 <epsekim01@gmail.com>
2024-06-10 21:55:35 -0400

    basic containers laid out for the website

--- schedulermap/src/App.tsx
+++ schedulermap/src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div id="wholePage">
+        <div id="topContainer">
+          <div id="navBarContainer">
+            <div id="titleContainer"></div>
+            <div id="userContainer"></div>
+          </div>
+        </div>
+        <div id="leftContainer"></div>
+        <div id="calendarContainer"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
